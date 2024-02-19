07b9ff948aab8a29d10ebd2d6dffc5cfca3c5060
Samuel Borges <borges.sb@protonmail.com>
2024-02-19 15:41:01 +0100

    Update JobSection.vue

--- src/components/JobSection.vue
+++ src/components/JobSection.vue
@@ -12,10 +12,17 @@ import {
   <div class="px-2 md:px-4 lg:px-10">
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
       <JobCard
-        :name="'Merlion IT'"
+        :name="'ZooStation'"
+        :title="'Frontend Developer'"
+        :periods="[['Oct 2022', 'Ongoing']]"
+        :details="zooDetails"
+        :location="'The Hague, Netherlands'"
+      ></JobCard>
+      <JobCard
+        :name="'Flux Bank'"
         :title="'Fullstack Developer'"
-        :periods="[['Jan 2019', 'Feb 2021']]"
-        :details="merlionDetails"
+        :periods="[['Jul 2021', 'Oct 2022']]"
+        :details="fluxDetails"
         :location="'Brasília, Brazil'"
       ></JobCard>
       <JobCard
@@ -28,18 +35,11 @@ import {
         :location="'Brasília, Brazil'"
       ></JobCard>
       <JobCard
-        :name="'Flux Bank'"
+        :name="'Merlion IT'"
         :title="'Fullstack Developer'"
-        :periods="[['Jul 2021', 'Oct 2022']]"
-        :details="fluxDetails"
+        :periods="[['Jan 2019', 'Feb 2021']]"
+        :details="merlionDetails"
         :location="'Brasília, Brazil'"
-      ></JobCard>
-      <JobCard
-        :name="'ZooStation'"
-        :title="'Frontend Developer'"
-        :periods="[['Oct 2022', 'Ongoing']]"
-        :details="zooDetails"
-        :location="'The Hague, Netherlands'"
       ></JobCard>
     </div>
   </div>
